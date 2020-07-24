72f331af7d2ddfc91c8c8f6b4bd8dfe6ca804771
CIA-PC <faisal.jamil@mail.utoronto.ca>
2020-07-24 00:36:38 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+const log = console.log
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/pub'))
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	log(`Listening on port ${port}...`)
+}) 
