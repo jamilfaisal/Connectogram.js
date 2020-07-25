5e0dd81eb89db2fc13bb5ece35e06bd78ce78201
CIA-PC <faisal.jamil@mail.utoronto.ca>
2020-07-25 09:51:04 -0500

    Added attribute changer methods

--- pub/js/connectogram.js
+++ pub/js/connectogram.js
@@ -33,13 +33,13 @@ class Connectogram {
         }
         let newBlob;
         if (shape === "circle" && size['radius']) {
-            newBlob = new CircleBlob(name, shape, color, borderColor, x, y, size['radius'])
+            newBlob = new CircleBlob(this, name, shape, color, borderColor, x, y, size['radius'])
         }
         else if (shape === "rectangle" && size['height'] && size['width']) {
-            newBlob = new RectBlob(name, shape, color, borderColor, x, y, size['height'], size['width'])
+            newBlob = new RectBlob(this, name, shape, color, borderColor, x, y, size['height'], size['width'])
         }
         else if (shape === "ellipse" && size['radiusx'] && size['radiusy']) {
-            newBlob = new EllipseBlob(name, shape, color, borderColor, x, y, size['radiusx'], size['radiusy'])
+            newBlob = new EllipseBlob(this, name, shape, color, borderColor, x, y, size['radiusx'], size['radiusy'])
         }
         else {
             log("Invalid parameters.")
@@ -48,6 +48,18 @@ class Connectogram {
         newBlob.html = addBlobtoDOM(this.root_html, newBlob)
         this.blobs.push(newBlob)
         return newBlob
+    }
+
+    removeBlob(name) {
+        const blob = this.getBlob(name);
+        if (blob === null) {
+            log("Blob not found.")
+            return null;
+        }
+        this.blobs = this.blobs.filter(function(blob) {
+            return blob !== blob
+        })
+        removeBlobFromDOM(blob);
     }
 
     getBlob(name) {
@@ -61,10 +73,39 @@ class Connectogram {
             return blob[0]
         }
     }
+
+    connect(blob1, blob2) {
+        if (this.edges[blob1] === blob2) {
+            log("The blobs are already connected1")
+            return null
+        }
+        this.edges[blob1] = blob2;
+
+    }
+
+    changeBlobName(oldName, newName) {
+        if (oldName === newName) {
+            log("Same name.")
+            return null
+        }
+        const blob = this.getBlob(oldName)
+        if (blob === null) {
+            log("Blob not found.")
+            return null;
+        }
+        if (this.getBlob(newName) !== null) {
+            log("Name already exists.")
+            return null
+        }
+        blob.name = newName;
+        log("Name Changed.")
+        return blob
+    }
 }
 
 class Blob {
-    constructor(name, shape, color, borderColor, x, y) {
+    constructor(cgram, name, shape, color, borderColor, x, y) {
+        this.cgram = cgram;
         this.name = name;
         this.shape = shape;
         this.x = x;
@@ -80,29 +121,94 @@ class Blob {
     addText(text) {
 
     }
+
+    changeName(newName) {
+        this.cgram.changeBlobName(this.name, newName);
+    }
+
+    changeColor(newColor=this.color) {
+        this.color = newColor;
+        changeBlobColor(this.html, newColor);
+    }
+
+    changeBorderColor(newColor=this.borderColor) {
+        this.borderColor = this.borderColor;
+        changeBlobBorderColor(this.html, newColor);
+    }
+
+    setPosition(newX=this.x, newY=this.y) {
+        this.x = newX;
+        this.y = newY;
+        setBlobPosition(this, newX, newY);
+    }
+
+    toggleHide() {
+        toggleBlobHide(this.html);
+    }
 }
 
 class CircleBlob extends Blob {
-    constructor(name, shape, color, borderColor, x, y, radius) {
-        super(name, shape, color, borderColor, x, y);
+    constructor(cgram, name, shape, color, borderColor, x, y, radius) {
+        super(cgram, name, shape, color, borderColor, x, y);
         this.radius = radius
+    }
+
+    changeRadius(newRadius=this.radius) {
+        this.radius = newRadius;
+        changeBlobRadius(this.html, newRadius);
     }
 }
 
 class RectBlob extends Blob {
-    constructor(name, shape, color, borderColor, x, y, height, width) {
-        super(name, shape, color, borderColor, x, y);
+    constructor(cgram, name, shape, color, borderColor, x, y, height, width) {
+        super(cgram, name, shape, color, borderColor, x, y);
         this.height = height
         this.width = width
+    }
+
+    changeWidth(newWidth=this.width) {
+        this.width = newWidth;
+        changeBlobWidth(this.html, newWidth)
+    }
+
+    changeHeight(newHeight=this.height) {
+        this.height = newHeight;
+        changeBlobHeight(this.html, newHeight)
     }
 }
 
 class EllipseBlob extends Blob {
-    constructor(name, shape, color, borderColor, x, y, radiusx, radiusy) {
-        super(name, shape, color, borderColor, x, y);
+    constructor(cgram, name, shape, color, borderColor, x, y, radiusx, radiusy) {
+        super(cgram, name, shape, color, borderColor, x, y);
         this.radiusx = radiusx;
         this.radiusy = radiusy;
     }
+
+    changeRadius(newRadiusx=this.radiusx, newRadiusy=this.radiusy) {
+        this.radiusx = newRadiusx;
+        this.radiusy = newRadiusy;
+        changeBlobRadiusxy(this.html, newRadiusx, newRadiusy)
+    }
+}
+
+class Edge {
+    constructor(type, from, to, color=null) {
+        this.type = type;
+        this.from = from;
+        this.to = to;
+        this.color = color;
+        this.x1 = 0;
+        this.y1 = 0;
+        this.x2 = 0;
+        this.y2 = 0;
+        calculatePositions()
+    }
+
+    calculatePositions() {
+
+    }
+
+
 }
 
 /**DOM Functions */
@@ -151,6 +257,58 @@ function addBlobtoDOM(root_html, blob) {
 function addTexttoBlob(blob) {
 
 }
+
+function addEdgetoDOM(edge) {
+
+}
+
+function changeBlobWidth(blobDom, newWidth) {
+    blobDom.attr("width", newWidth);
+}
+
+function changeBlobHeight(blobDom, newHeight) {
+    blobDom.attr("height", newHeight);
+}
+
+function changeBlobRadius(blobDom, newRadius) {
+    blobDom.attr("r", newRadius)
+}
+
+function changeBlobRadiusxy(blobDom, newRadiusx, newRadiusy) {
+    blobDom.attr("rx", newRadiusx).attr("ry", newRadiusy);
+}
+
+function changeBlobColor(blobDom, newColor) {
+    blobDom.attr("fill", newColor)
+}
+
+function changeBlobBorderColor(blobDom, newColor) {
+    blobDom.attr("stroke", newColor)
+}
+
+function setBlobPosition(blob, newX, newY) {
+    if (blob.shape === "rectangle") {
+        blob.html.attr("x", newX).attr("y", newY);
+    }
+    else {
+        blob.html.attr("cx", newX).attr("cy", newY);
+    }
+}
+
+function toggleBlobHide(blobDOM) {
+    if (blobDOM.attr('visibility') === "hidden") {
+        blobDOM.attr("visibility", "visibile")
+    }
+    else {
+        blobDOM.attr("visibility", "hidden")
+    }
+}
+
+function removeBlobFromDOM(blob) {
+    blobDOM = blob.html
+    blobDOM.remove();
+}
+
 
 /** Misc. Functions */
 
