ebe5724fad2e39a0734da697b5adae0d8653392a
CIA-PC <faisal.jamil@mail.utoronto.ca>
2020-08-20 23:42:55 -0500

    Heroku Update

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(express.static(__dirname + '/pub'))
 
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/pub/examples.html")
+})
+
 const port = process.env.PORT || 5000
 app.listen(port, () => {
 	log(`Listening on port ${port}...`)
