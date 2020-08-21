2a487d35ca576d9d828af49f30fa5d4619c6b77d
CIA-PC <faisal.jamil@mail.utoronto.ca>
2020-08-21 11:53:27 -0500

    Docsify Initial Upload

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.get("/testing", (req, res) => {
 	res.sendFile(__dirname + "/pub/testing.html")
 })
 
+app.get('/documentation', (req, res) => {
+	res.sendFile(__dirname + '/pub/docs/index.html')
+})
+
 const port = process.env.PORT || 5000
 app.listen(port, () => {
 	log(`Listening on port ${port}...`)
